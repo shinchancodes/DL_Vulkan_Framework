a25cef67cd47c28cf43bdee6732b3981bb8b0c32
Sourav Thakur <stsouravth23@gmail.com>
2026-03-04 23:23:09 +0530

    Implemented vanilla GEMM

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "vulkan_base/ComputePipeline.h"
 #include "vulkan_base/ComputePass.h"
 
-#include "layers/linear.h"
+#include "utils/gemm.h"
 
 #include <iostream>
 #include <vector>
@@ -15,20 +15,21 @@ int main() {
     ctx.init(/*enableValidation=*/true);
 
     std::vector<float> A = { 1, 2, 3, 4, 5, 6 }; // 2x3
-    std::vector<float> x = { 7, 8, 9 }; // 3x1
-    std::vector<float> bias = { 0.5, 0.5 }; // 2x1
-    uint32_t M = 2, K = 3;
-    
-    Linear linear(ctx, K, M);
-    linear.setWeights(A, bias);
-    std::vector<float> C = linear.run(x);
-    
+    std::vector<float> B = { 7, 8, 9 , 10, 11, 12 }; // 3x2
+    uint32_t M = 2, K = 3, N = 2;
+
+    GEMM gemm(ctx);
+
+    std::vector<float> C = gemm.run(A, B, M, K, N);
+
     // --- Print result ---
-    std::cout << "Result C = A * x:" << std::endl;
+    std::cout << "Result C = A * B:" << std::endl;
     for (uint32_t i = 0; i < M; ++i) {
-        std::cout << C[i] << " ";
+        for (uint32_t j = 0; j < N; ++j) {
+            std::cout << C[i * N + j] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
 
     // --- Cleanup ---
     ctx.destroy();
